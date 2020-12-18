abb48b62fae12a7c7a29f4a1b44ca51b3f377518
ponkantaichou <banshee7577@gmail.com>
2020-12-18 18:53:09 +0900

    フォーム

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,16 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test', 'TestController@index');
+
+
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+    Route::get('create', 'ContactFormController@create')->name('contact.create');
+});
+
+// REST
+// Route::resource('contacts', 'ContactFormController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
