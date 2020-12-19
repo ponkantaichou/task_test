70a0af35c5a95b04a8472084f67bd21704ac3bdc
ponkantaichou <banshee7577@gmail.com>
2020-12-19 12:08:13 +0900

    show実装

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\ContactForm;
+
+use Illuminate\Support\Facades\DB;
+
 class ContactFormController extends Controller
 {
     /**
@@ -13,8 +17,18 @@ class ContactFormController extends Controller
      */
     public function index()
     {
-        //
-        return view('contact.index');
+        // エロクワント　ORマッパー
+        // $contacts = ContactForm::all();
+
+        // クエリビルダ
+        $contacts = DB::table('contact_forms')
+        ->select('id', 'your_name', 'title', 'created_at')
+        ->orderBy('created_at', 'asc')
+        ->get();
+
+        // dd($contacts);
+
+        return view('contact.index', compact('contacts'));
     }
 
     /**
@@ -36,7 +50,22 @@ class ContactFormController extends Controller
      */
     public function store(Request $request)
     {
+        $contact = new ContactForm;
         //
+        $contact->your_name = $request->input('your_name');
+        $contact->title = $request->input('title');
+        $contact->email = $request->input('email');
+        $contact->url = $request->input('url');
+        $contact->gender = $request->input('gender');
+        $contact->age = $request->input('age');
+        $contact->contact = $request->input('contact');
+
+        $contact->save();
+
+        return redirect('contact/index');
+
+        // dd($your_name);
+
     }
 
     /**
@@ -48,6 +77,41 @@ class ContactFormController extends Controller
     public function show($id)
     {
         //
+        $contact = ContactForm::find($id);
+
+        if ($contact->gender === 0) {
+            $gender = '男性';
+        }
+        if ($contact->gender === 1) {
+            $gender = '女性';
+        }
+
+        if ($contact->age === 1) {
+            $age = '〜19歳';
+        }
+
+        if ($contact->age === 2) {
+            $age = '20歳〜29歳';
+        }
+
+        if ($contact->age === 3) {
+            $age = '30歳〜39歳';
+        }
+
+        if ($contact->age === 4) {
+            $age = '40歳〜49歳';
+        }
+
+        if ($contact->age === 5) {
+            $age = '50歳〜59歳';
+        }
+
+        if ($contact->age === 6) {
+            $age = '60歳〜';
+        }
+
+        return view('contact.show',
+        compact('contact', 'gender', 'age'));
     }
 
     /**
@@ -59,6 +123,7 @@ class ContactFormController extends Controller
     public function edit($id)
     {
         //
+        
     }
 
     /**
